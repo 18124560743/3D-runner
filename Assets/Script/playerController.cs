8e0e1a6621f7bd8b78ee7689a3cd92e5e171c4d7
zl <zl@LENOVO-PC>
2020-01-22 11:13:33 +0800

    代码提交1

--- Assets/Script/playerController.cs
+++ Assets/Script/playerController.cs
@@ -1,21 +1,24 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class playerController : MonoBehaviour
-{public float speed=1;
+{
+    public GameObject floorOnRunning;
+    public GameObject floorForwand;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("1111111");
+
     }
 
-    // Update is called once per frame
     void Update()
-    { transform.Translate(new Vector3(0, 0,speed * Time.deltaTime));
+    {
+        if (transform.position.z > floorOnRunning.transform.position.z + 32)
+        {
 
-
-
-        
+        }
     }
-}
+}    
+    
+
