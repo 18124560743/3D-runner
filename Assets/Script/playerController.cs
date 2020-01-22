2527c393d8d33954c681ae525a7aba8bce507174
zl <zl@LENOVO-PC>
2020-01-22 10:16:04 +0800

    初始化项目

---
+++ Assets/Script/playerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{public float speed=1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("1111111");
+    }
+
+    // Update is called once per frame
+    void Update()
+    { transform.Translate(new Vector3(0, 0,speed * Time.deltaTime));
+
+
+
+        
+    }
+}
