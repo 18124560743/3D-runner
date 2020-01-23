21fae8bd494d9dfaf0a1757da00c429e1b0eb753
zl <zl@LENOVO-PC>
2020-01-23 20:02:58 +0800

    动画播放1

---
+++ Assets/Script/AnimationManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager : MonoBehaviour
+{
+    Animation animation;
+    public AnimationClip Dead;
+    public AnimationClip JumpDown;
+    public AnimationClip JumpLoop;
+    public AnimationClip JumpUp;
+    public AnimationClip Roll;
+    public AnimationClip Run;
+    public AnimationClip TurnLeft;
+    public AnimationClip TurnRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animation = GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animation.Play();
+    }
+}
